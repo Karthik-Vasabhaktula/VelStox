1cc129c1c790234cc1832655574a375829d83f44
Karthik Vasabhaktula <98506179+Karthik-Vasabhaktula@users.noreply.github.com>
2026-05-23 10:31:35 +0530

    Updated Brokerage

--- frontend/src/landing_page/pricing/Brokerage.js
+++ frontend/src/landing_page/pricing/Brokerage.js
@@ -5,7 +5,7 @@ function Brokerage() {
     <div className="container">
       <div className="row p-5 mt-5 text-center border-top">
         <div className="col-8 p-4">
-          <a href="" style={{ textDecoration: "none" }}>
+          <a href="/" style={{ textDecoration: "none" }}>
             <h3 className="fs-5">Brokerage calculator</h3>
           </a>
           <ul
@@ -34,11 +34,6 @@ function Brokerage() {
               charged ₹40 per executed order instead of ₹20 per executed order.
             </li>
           </ul>
-        </div>
-        <div className="col-4 p-4">
-          <a href="" style={{ textDecoration: "none" }}>
-            <h3 className="fs-5">List of charges</h3>
-          </a>
         </div>
       </div>
     </div>
