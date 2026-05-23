1ffd08f6e4e30e0fbab56091fb559acf75a9a8f2
Karthik Vasabhaktula <98506179+Karthik-Vasabhaktula@users.noreply.github.com>
2026-05-23 09:29:50 +0530

    production cookie fixes

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -43,14 +43,14 @@ export const login= async (req,res)=>{
         
         res.cookie("accessToken", accessToken, {
             httpOnly: true,
-            secure: false,
-            sameSite: "lax",
+            secure: true,
+            sameSite: "none",
             maxAge: 15 * 60 * 1000,
         });
         res.cookie("refreshToken", refreshToken, {
             httpOnly: true,
-            secure: false,
-            sameSite: "lax",
+            secure: true,
+            sameSite: "none",
             maxAge: 7*24*60*60*1000,
         });
 
@@ -70,8 +70,17 @@ export const login= async (req,res)=>{
 
 
 export const logout= async (req, res)=>{
-    res.clearCookie("accessToken");
-    res.clearCookie("refreshToken");
+    res.clearCookie("accessToken", {
+        httpOnly: true,
+        secure: true,
+        sameSite: "none",
+    });
+
+    res.clearCookie("refreshToken", {
+        httpOnly: true,
+        secure: true,
+        sameSite: "none",
+    });
     res.status(200).json({
         message: "Logout successful",
     });
