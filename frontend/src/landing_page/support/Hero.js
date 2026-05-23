1bcfc2cf7d491779fb079939b1faeb5c85c8c368
Karthik Vasabhaktula <98506179+Karthik-Vasabhaktula@users.noreply.github.com>
2026-05-23 11:05:30 +0530

    Remove href attributes from anchor tags in Hero.js

--- frontend/src/landing_page/support/Hero.js
+++ frontend/src/landing_page/support/Hero.js
@@ -14,18 +14,18 @@ function Hero() {
           </h1>
           <input placeholder="Eg. how do I activate F&O" />
           <br />
-          <a href="/">Track account opening</a><br/>
-          <a href="/">Track segment activation</a><br/>
-          <a href="/">Intraday margins</a><br/>
+          <a href="">Track account opening</a><br/>
+          <a href="">Track segment activation</a><br/>
+          <a href="">Intraday margins</a><br/>
         </div>
         <div className="col-6 p-3">
           <h1 className="fs-3">Featured</h1>
           <ol>
             <li>
-              <a href="/">Current Takeovers and Delisting - Feb 2026</a>
+              <a href="">Current Takeovers and Delisting - Feb 2026</a>
             </li>
             <li>
-              <a href="/">Latest Intraday leverages - MIS & CO</a>
+              <a href="">Latest Intraday leverages - MIS & CO</a>
             </li>
           </ol>
         </div>
