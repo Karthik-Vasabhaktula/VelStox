052b1956f10d0b544290586fa7841af35995a0ff
Karthik Vasabhaktula <98506179+Karthik-Vasabhaktula@users.noreply.github.com>
2026-05-23 10:53:46 +0530

    Fix alt attribute syntax for image in RightSection.js

--- frontend/src/landing_page/products/RightSection.js
+++ frontend/src/landing_page/products/RightSection.js
@@ -12,7 +12,7 @@ function RightSection({ imageURL, productName, productDesription, learnMore }) {
           </div>
         </div>
         <div className="col-6">
-          <img src={imageURL} alt=image url""/>
+          <img src={imageURL} alt="image url"/>
         </div>
       </div>
     </div>
